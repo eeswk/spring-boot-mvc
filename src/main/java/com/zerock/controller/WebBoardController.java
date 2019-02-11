95d2354a76a026fd76a5a840167de38762012dfa
themorning <themorning84@gmail.com>
2019-02-11 17:20:59 +0900

    CRUD 완료

--- src/main/java/com/zerock/controller/WebBoardController.java
+++ src/main/java/com/zerock/controller/WebBoardController.java
@@ -9,6 +9,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.zerock.domain.WebBoard;
@@ -17,6 +19,7 @@ import com.zerock.vo.PageMaker;
 import com.zerock.vo.PageVO;
 
 import lombok.extern.java.Log;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/boards/")
@@ -51,6 +54,68 @@ public class WebBoardController {
 		
 		
 		model.addAttribute("result", new PageMaker(result));
+	}
+
+	@GetMapping("/register")
+	public void regsiterGET(@ModelAttribute("vo") WebBoard vo) {
+		log.info("register get");
+	}
+
+	@PostMapping("/register")
+	public String regsterPOST(@ModelAttribute("vo") WebBoard vo, RedirectAttributes rttr) {
+		log.info("register post");
+		log.info("" + vo);
+
+		repo.save(vo);
+		rttr.addFlashAttribute("msg", "success");
+		return "redirect:/boards/list";
+	}
+
+	@GetMapping("/view")
+	public void view(Long bno, @ModelAttribute("pageVO") PageVO vo, Model model) {
+		log.info("" + bno);
+		repo.findById(bno).ifPresent(board -> model.addAttribute("vo", board));
+	}
+
+	@GetMapping("/modify")
+	public void modify(Long bno, @ModelAttribute("pageVO") PageVO vo, Model model) {
+		log.info("modify bno: " + bno);
+		repo.findById(bno).ifPresent(board -> model.addAttribute("vo", board));
+	}
+
+	@PostMapping("/delete")
+	public String delete(Long bno, PageVO vo, RedirectAttributes rttr) {
+		log.info("delete bno: " + bno);
+		repo.deleteById(bno);
+		rttr.addFlashAttribute("msg","success");
+		rttr.addFlashAttribute("page", vo.getPage());
+		rttr.addFlashAttribute("size", vo.getSize());
+		rttr.addFlashAttribute("type", vo.getType());
+		rttr.addFlashAttribute("keyword", vo.getKeyword());
+
+		return "redirect:/boards/list";
+	}
+
+	@PostMapping("/modify")
+	public String modifyPost(WebBoard board, PageVO vo, RedirectAttributes rttr) {
+		log.info("Modify WebBoard: " + board);
+
+		repo.findById(board.getBno()).ifPresent(origin -> {
+			origin.setTitle(board.getTitle());
+			origin.setContent(board.getContent());
+
+			repo.save(origin);
+			rttr.addFlashAttribute("msg", "success");
+			rttr.addFlashAttribute("bno", origin.getBno());
+		});
+
+		rttr.addFlashAttribute("page", vo.getPage());
+		rttr.addFlashAttribute("size", vo.getSize());
+		rttr.addFlashAttribute("type", vo.getType());
+		rttr.addFlashAttribute("keyowrd", vo.getKeyword());
+
+		return "redirect:/boards/list";
+
 	}
 	
 	
